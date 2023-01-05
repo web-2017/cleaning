f6012e58bb8aff773a6888d53d0e8f2e39e129cd
web-2017 <webdevelope2017@gmail.com>
2023-01-04 19:58:21 -0600

    about us

--- src/components/SectionAbout.tsx
+++ src/components/SectionAbout.tsx
@@ -1,15 +1,25 @@
+import React from 'react'
 import { useTheme } from '@mui/material/styles'
 import { Box, Grid } from '@mui/material'
-import React from 'react'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 import { CustomImage, Title, Text, CustomButton } from '@/ui'
 import { COLORS } from '../utils/COLORS'
+
+function srcset(image: string, size: number, rows = 1, cols = 1) {
+	return {
+		src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+		srcSet: `${image}?w=${size * cols}&h=${size * rows
+			}&fit=crop&auto=format&dpr=2 2x`,
+	};
+}
 
 export const SectionAbout = () => {
 	const theme = useTheme()
 	return (
 		<Grid container xs={12} mt={4} spacing={2} sx={{}}>
-			<Grid item xs={12} sm={6}>
+			<Grid item xs={12} sm={6} style={{ alignSelf: 'center' }}>
 				<Box
 					component={'h4'}
 					style={{
@@ -36,9 +46,53 @@ export const SectionAbout = () => {
 				</Text>
 				<CustomButton variant='contained' text='Our Services' />
 			</Grid>
-			<Grid item xs={12} sm={6}>
-				<Text>about us</Text>
+			<Grid item xs={12} sm={6} style={{ alignSelf: 'center' }}>
+				<ImageList
+					// sx={{ width: 500, height: 450, }}
+					variant="quilted"
+					cols={4}
+					rowHeight={150}
+				>
+					{itemData.map((item) => (
+						<ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+							<img
+								{...srcset(item.img, 0, item.rows, item.cols)}
+								alt={item.title}
+								loading="lazy"
+							/>
+						</ImageListItem>
+					))}
+				</ImageList>
 			</Grid>
-		</Grid>
+		</Grid >
 	)
 }
+
+const itemData = [
+	{
+		img: '/other/Rectangle 8.png',
+		title: 'Burger',
+		rows: 2,
+		cols: 2,
+	},
+	{
+		img: '/other/Rectangle 9.png',
+		title: 'Burger',
+		rows: 1,
+		cols: 2,
+	},
+	{
+		img: '/other/Rectangle 10.png',
+		title: 'Camera',
+		rows: 2,
+		cols: 2,
+	},
+	{
+		img: '/other/Rectangle 11.png',
+		title: 'Coffee',
+		rows: 1,
+		cols: 2,
+	},
+
+
+];
