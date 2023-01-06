2091849796cf52a23286e62ca024003e475031ff
Mario Magomedov <webdevelope2017@gmail.com>
2023-01-06 17:00:34 -0600

    title

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -2,16 +2,42 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import { CustomCard } from '@/ui'
 
+const fakeStaff = [
+	{
+		name: 'Devon Lane',
+		profession: 'Window cleaner',
+		src: '/persons/user1.png',
+	},
+	{
+		name: 'Cody Fisher',
+		profession: 'House Cleaner',
+		src: '/persons/user2.png',
+	},
+	{
+		name: 'Ralph Edwards',
+		profession: 'Commercial Cleaner',
+		src: '/persons/user3.png',
+	},
+	{
+		name: 'Bessie Cooper',
+		profession: 'Floor cleaner',
+		src: '/persons/user4.png',
+	},
+]
+
 const OurTeam = () => {
 	return (
-		<Grid container>
-			<Grid item={true} xs={6} md={4}>
-				<CustomCard
-					title='Devon Lane'
-					text={'Window cleaner'}
-					img={'/persons/user1.png'}
-				/>
-			</Grid>
+		<Grid container spacing={2}>
+			{fakeStaff?.map((person, index) => (
+				<Grid item={true} xs={6} md={3} key={index}>
+					<CustomCard
+						style={{ textAlign: 'center' }}
+						title={person.name}
+						text={person.profession}
+						img={person.src}
+					/>
+				</Grid>
+			))}
 		</Grid>
 	)
 }
